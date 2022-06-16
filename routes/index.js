f105a4890f805602e4557548cc6c3b327c141248
Emma Wahnish <ewahnish@gmail.com>
2022-06-16 16:54:47 -0300

    Handlebars

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  req.body = {
+    nombre: 'Nazarena',
+    apellido: 'Gravillon',
+    edad: 30,
+    email: 'nazarena.gravillon@gmail.com',
+    telefono:'4666-1111'
+  }  
+  res.render('index', req.body);
 });
 
 module.exports = router;
